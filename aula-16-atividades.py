70c62d4fd7852544a031eb7f317b8bd1dfbf423c
samuel gurgel <samuelhrgg@gmail.com>
2026-05-11 20:54:11 -0300

    Exercícios Part 2

--- aula-16-atividades.py
+++ aula-16-atividades.py
@@ -6,11 +6,11 @@ B. Faça um programa que peça o ano do seu nascimento e calcule quantos anos vo
 C. Faça um programa que peça 4 notas, após a entrada de dados calcular a média das notas digitadas.
 D#. Faça um programa que receba e divida 2 números. A saída da divisão precisará ser formatada com 4 casas decimais.
 E. Faça um programa que receba um número inteiro e mostre o sucessor e antecessor.
-H. Faça um programa que receba um número qualquer e calcule o dobro e o triplo desse número.
-I. Faça um programa que converta metros em centímetros e milímetros.
-J. Faça um programa que receba um número inteiro, depois imprima sua tabuada de multiplicação.
-K. Faça um programa que receba um valor em reais, depois calcule quantos dólares daria para comprar com esse valor.
-L#. Faça um programa com entrada de dados para calcular o perímetro de um retângulo.
+F. Faça um programa que receba um número qualquer e calcule o dobro e o triplo desse número.
+G. Faça um programa que converta metros em centímetros e milímetros.
+H. Faça um programa que receba um número inteiro, depois imprima sua tabuada de multiplicação.
+I. Faça um programa que receba um valor em reais, depois calcule quantos dólares daria para comprar com esse valor.
+J#. Faça um programa com entrada de dados para calcular o perímetro de um retângulo.
 
 Observação
 Faça um arquivo '.py' para cada atividade. 
