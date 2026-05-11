f79e63d7b39d76bde4313b937a501fdf00c7cae6
samuel gurgel <samuelhrgg@gmail.com>
2026-05-11 19:19:04 -0300

    Exercício Part 1

---
+++ aula-16-atividades.py
@@ -0,0 +1,21 @@
+"""
+Atividade 001 - Operadores aritméticos
+
+A. Faça um programa que peça 3 valores , depois calcule e imprima  a soma e a multiplicação desses valores. 
+B. Faça um programa que peça o ano do seu nascimento e calcule quantos anos você faz esse ano.
+C. Faça um programa que peça 4 notas, após a entrada de dados calcular a média das notas digitadas.
+D#. Faça um programa que receba e divida 2 números. A saída da divisão precisará ser formatada com 4 casas decimais.
+E. Faça um programa que receba um número inteiro e mostre o sucessor e antecessor.
+
+Observação
+É obrigatório a saída formatada!
+Variáveis bem definidas
+O código precisa ter o cabeçalho padrão
+o código precisa ser comentado
+Enviar somente os exercícios que possuem '#'
+
+"""
+
+#import datetime
+#ano_atual = datime.datetime.now().year
+#idade = int(ano_atual)-nascimento
